d692272a7e373aaf60d84505ce4493e66e75df99
bykim17 <piyawat800@gmail.com>
2022-05-26 13:02:31 +0700

    no message

--- src/resolvers/mutationstore/chill.js
+++ src/resolvers/mutationstore/chill.js
@@ -6,23 +6,23 @@ import Imhalve from "../../models/Beefstore/imhalve";
 import Chillday from "../../models/Beefstore/chillday";
 
 const Mutation = {
-    createChill: async (parent, args, { userId }, info) => {
-        if (!userId) throw new Error("Please log in.");
-        
+  createChill: async (parent, args, { userId }, info) => {
+    if (!userId) throw new Error("Please log in.");
+
     if (!args.barcode || !args.chillroom || !args.chillday) {
-        throw new Error("Please provide all required fields.");
-    }   
+      throw new Error("Please provide all required fields.");
+    }
 
     const DateNow = dayjs().toISOString();
 
     const halve = await Halve.findOne({
-        barcode: args.barcode,
+      barcode: args.barcode,
     });
 
-    const find = await Chillday.findById(args.chillday)
-    const chillday = Number(find.day)
-    
-    const statusCh = "6284ad73fbfac22364a6e430"
+    const find = await Chillday.findById(args.chillday);
+    const chillday = Number(find.day);
+
+    const statusCh = "6284ad73fbfac22364a6e430";
 
     const dateEnd = dayjs().add(chillday, "day").toISOString();
 
@@ -30,88 +30,82 @@ const Mutation = {
         barcode: args.barcode},
         {storestatus: statusCh }) */
 
-        //await Imhalve.findOneAndUpdate({barcode: args.barcode},{storestatus : statusCh})
+    //await Imhalve.findOneAndUpdate({barcode: args.barcode},{storestatus : statusCh})
 
     const chill = await Chill.create({
-        halve: halve,
-        beeftype: halve.beeftype,
-        chillroom: args.chillroom,
-        chilldateStart: DateNow,
-        chilldateEnd: dateEnd,
-        chillday: find,
-        user: userId,
-        barcode: args.barcode,
-        chillstatus: statusCh
+      halve: halve,
+      beeftype: halve.beeftype,
+      chillroom: args.chillroom,
+      chilldateStart: DateNow,
+      chilldateEnd: dateEnd,
+      chillday: find,
+      user: userId,
+      barcode: args.barcode,
+      chillstatus: statusCh,
     });
 
     const halves = await Halve.findById(halve.id);
-    if(!halves.chill){
-        halves.chill = [chill];
+    if (!halves.chill) {
+      halves.chill = [chill];
     } else {
-        halves.chill.push(chill);
+      halves.chill.push(chill);
     }
     await halves.save();
 
-
     let test = Chill.findById(chill.id)
-    .populate({
+      .populate({
         path: "halve",
         populate: { path: "imslaughter" },
-    })
-    .populate({
+      })
+      .populate({
         path: "halve",
         populate: { path: "beeftype" },
-    })
-    .populate({
+      })
+      .populate({
         path: "halve",
         populate: { path: "status" },
-    })
-    .populate({
-        path: "chillroom"
-    })
-    .populate({
+      })
+      .populate({
+        path: "chillroom",
+      })
+      .populate({
         path: "chillstatus",
-    })
-    .populate({
+      })
+      .populate({
         path: "chillday",
-    })
-    return test
-    
-    },
+      });
+    return test;
+  },
 
-    updateChillday: async (parent, args, { userId }, info) => {
-        const { id } = args;
+  updateChillday: async (parent, args, { userId }, info) => {
+    const { id } = args;
 
-        if (!userId) throw new Error("Please log in.");
+    if (!userId) throw new Error("Please log in.");
 
-        //const chill = await Chill.findById(id);
-        
-        /* const dateEnd = chill.chilldateEnd
-        const chillenddate = (dayjs(dateEnd).format("YYYYMMDD").toString())
+    const chill = await Chill.findById(id);
 
-        const checkdate =  (dayjs().format("YYYYMMDD").toString());
-        
-        const checkchilldate =  checkdate >= chillenddate */
-        
-        const statusCh = "6284ad91fbfac22364a6e431";
+    const dateEnd = chill.chilldateEnd;
+    const chillenddate = dayjs(dateEnd).format("YYYYMMDD").toString();
 
-        await Chill.findByIdAndUpdate(id, {chillstatus: statusCh} );
-        /* f(checkchilldate){
-            const statusCh = "6284ad91fbfac22364a6e431";
+    const checkdate = dayjs().format("YYYYMMDD").toString();
 
-            await Chill.findByIdAndUpdate(id, {chillstatus: statusCh} );
-        }else {
-            throw new Error("ซากโคผ่าซีกกำลังบ่ม")
-        } */
-        const updatedFinish = await Chill.findById(id)
-        .populate({
-            path: "chillstatus"
-        })
+    const checkchilldate = checkdate >= chillenddate;
 
-        return updatedFinish;
-    },
+    const statusCh = "6284ad91fbfac22364a6e431";
 
+    await Chill.findByIdAndUpdate(id, { chillstatus: statusCh });
+    if (checkchilldate) {
+      const statusCh = "6284ad91fbfac22364a6e431";
 
+      await Chill.findByIdAndUpdate(id, { chillstatus: statusCh });
+    } else {
+      throw new Error("ซากโคผ่าซีกกำลังบ่ม");
+    }
+    const updatedFinish = await Chill.findById(id).populate({
+      path: "chillstatus",
+    });
 
+    return updatedFinish;
+  },
 };
-export default Mutation
+export default Mutation;
