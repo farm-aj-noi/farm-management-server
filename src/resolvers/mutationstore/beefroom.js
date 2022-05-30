24459d16e7e1e78d50a5ebfe35ff1d7fdda1a438
Pakky1234 <Pp_re555@outlook.com>
2022-05-30 15:22:38 +0700

    no message

--- src/resolvers/mutationstore/beefroom.js
+++ src/resolvers/mutationstore/beefroom.js
@@ -1,4 +1,7 @@
+import Basket from "../../models/Beefstore/basket";
 import Beefroom from "../../models/Beefstore/beefroom";
+import Shelf from "../../models/Beefstore/shelf";
+import Typekeep from "../../models/Beefstore/typekeep";
 
 const Mutation = {
   createBeefroom: async (parent, args, { userId }, info) => {
@@ -16,9 +19,32 @@ const Mutation = {
   },
 
   deleteBeefroom: async (parent, args, { userId }, info) => {
-    if (!userId) throw new Error("Please log in.");
+    //if (!userId) throw new Error("Please log in.");
 
-    const deleteroom = await Beefroom.findByIdAndDelete(args.id);
+    const room = await Beefroom.findById(args.id);
+    const shelfs = room.shelf;
+    const basket = room.basket;
+    const roomtypekeep = room.typekeep;
+
+    const shelftypekeep = await Typekeep.find({shelf: shelfs})
+    
+    for (let i = 0; i < shelfs.length; i++) {
+      await Shelf.findByIdAndDelete(shelfs[i]);
+    }
+
+    for (let i = 0; i < basket.length; i++) {
+      await Basket.findByIdAndDelete(basket[i]);
+    }
+
+    for (let i = 0; i < roomtypekeep.length; i++) {
+      await Typekeep.findByIdAndDelete(roomtypekeep[i]);
+    }
+
+    for (let i = 0; i < shelftypekeep.length; i++) {
+      await Typekeep.findByIdAndDelete(shelftypekeep[i]);
+    }
+
+    const deleteroom = await Beefroom.findByIdAndDelete(room);
 
     return deleteroom;
   },
