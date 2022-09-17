03e7541fd30f76b2b9eaf8cea12c473de211906b
Pakky1234 <Pp_re555@outlook.com>
2022-09-17 13:09:59 +0700

    no message

--- src/resolvers/mutationstore/reportset.js
+++ src/resolvers/mutationstore/reportset.js
@@ -8,5 +8,20 @@ const Mutation = {
     });
     return reportset;
   },
+
+  updateLogo: async (parent, args, { userId }, info) => {
+    const { logo, address } = args;
+
+    const logoupdate = await ReportSet.findById("632552264a989456302bd417");
+
+    const updateInfo = {
+      logo: !!logo ? logo : logoupdate.logo,
+      address: !!address ? address : logoupdate.address,
+    };
+
+    await ReportSet.findByIdAndUpdate("632552264a989456302bd417", updateInfo)
+    const updatedFinish = await ReportSet.findById("632552264a989456302bd417");
+    return updatedFinish;
+  },
 };
 export default Mutation;
